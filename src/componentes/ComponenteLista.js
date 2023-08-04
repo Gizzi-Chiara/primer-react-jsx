adf72d4d1da04775df191e5b4947cfed64334661
Chiara Gizzi <chiaragizzi1@gmail.com>
2023-08-03 19:32:43 -0600

    Completo

---
+++ src/componentes/ComponenteLista.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class ComponenteLista extends React.Component {
+    render () {
+        return (
+            <div className="card">
+            <h1>Hello Dojo!</h1>
+            <h2>Thinds I need to do:</h2>
+            <ul>
+                <li>Learn React</li>
+                <li>Climb Mt. Everest</li>
+                <li>Run a Marathon</li>
+                <li>Feed the dogs</li>
+            </ul>
+        </div>
+        )
+    }
+} 
+
+export default ComponenteLista;
